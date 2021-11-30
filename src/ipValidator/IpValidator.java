05474ce1707d2948fba41900ed087e86abb7fa58
opilane <opilane@HP-CZC6297B8B>
2021-11-30 17:17:25 +0200

    Update ValidateIpv4Address and add check for dots in ipString

--- src/ipValidator/IpValidator.java
+++ src/ipValidator/IpValidator.java
@@ -1,14 +1,16 @@
 package ipValidator;
 
-import java.util.function.BooleanSupplier;
-
 public class IpValidator {
 
 	public Boolean ValidateIpv4Address(String ipString) {
-		if(ipString == "") {
-			return false;
+		if(hasThreeDots(ipString)) {
+			return true;
 		}
-		return true;
+		return false;
+	}
+
+	private boolean hasThreeDots(String ipString) {
+		return ipString.chars().filter(c->c=='.').count()==3;
 	}
 
 }
